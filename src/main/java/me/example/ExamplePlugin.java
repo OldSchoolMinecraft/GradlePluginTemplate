b073ff1e973fa71b75a192394157a6f395daaa4c
moderatorman <12193288+moderatorman@users.noreply.github.com>
2022-04-03 18:16:29 -0700

    Initial commit

---
+++ src/main/java/me/example/ExamplePlugin.java
@@ -0,0 +1,16 @@
+package me.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExamplePlugin extends JavaPlugin
+{
+    public void onEnable()
+    {
+        // ...
+    }
+
+    public void onDisable()
+    {
+        // ...
+    }
+}
